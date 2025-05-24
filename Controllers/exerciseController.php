af35945992c507629b958af43912cc60a0c2571f
Ascynx <miaoum2128@gmail.com>
2025-05-24 09:23:35 +0200

    fix old php parse_str deprecated error

--- Controllers/exerciseController.php
+++ Controllers/exerciseController.php
@@ -31,38 +31,41 @@
             echo("Error, could not find exercise for ID: " . $exercise);
         }
         $querystr = parse_url($uri, PHP_URL_QUERY);
-        parse_str($querystr, $queryarr);
+        if ($querystr != null) {
+            parse_str($querystr, $queryarr);
 
-        if ((isset($queryarr['check']) && $queryarr['check'] == '1') && isset($_POST["logic_transfer"])) {
-            $valuesString = $_POST["logic_transfer"];
+            if ((isset($queryarr['check']) && $queryarr['check'] == '1') && isset($_POST["logic_transfer"])) {
+                $valuesString = $_POST["logic_transfer"];
 
-            $valuesArray = explode(";", $valuesString);
+                $valuesArray = explode(";", $valuesString);
             
-            $toutCorrect = true;
-            $score = 0;
-            foreach($valuesArray as $value) {
-                if (empty($value)) {
-                    continue;
-                }
-                //k=v
-                $kv = explode("=", $value);
-                $k = $kv[0];
-                $v = $kv[1];
-
-                $indexes = fromBitIndex($k);
-                $containerId = $indexes['containerId'];
-                $elementId = $indexes['elementId'];
-                if (!((new ComponentPosition($containerId, $elementId))->matches($EXERCICES_VERIFICATION[$exercise][$v]))) {
-                    $toutCorrect = false;
-                    break;
-                } else {
-                    $score++;
-                }
+                $toutCorrect = true;
+                $score = 0;
+                foreach($valuesArray as $value) {
+                    if (empty($value)) {
+                        continue;
+                    }
+                    //k=v
+                   $kv = explode("=", $value);
+                    $k = $kv[0];
+                    $v = $kv[1];
+                
+                    $indexes = fromBitIndex($k);
+                    $containerId = $indexes['containerId'];
+                    $elementId = $indexes['elementId'];
+                 if (!((new ComponentPosition($containerId, $elementId))->matches($EXERCICES_VERIFICATION[$exercise][$v]))) {
+                        $toutCorrect = false;
+                        break;
+                    } else {
+                        $score++;
+                    }
             }
             $score = $score / count($EXERCICES_VERIFICATION[$exercise]);
             $score = round($score * 100, 2);
             $score = $score . "%";
         }
+        }
+
 
         $elements = [];
         for ($i = 0; $i < count($COMPONENTS[$exercise]); $i++) {
